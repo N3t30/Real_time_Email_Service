bf16129e58e05786d5bdd84fdbce54efe34dfd4b
Neto <joseneto55peixoto@gmail.com>
2024-11-06 20:21:47 -0300

    Adicionando models e o asgi no notifications

--- real_time/real_time/asgi.py
+++ real_time/real_time/asgi.py
@@ -1,16 +1,15 @@
-"""
-ASGI config for real_time project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_time.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            # Roteamento do WebSocket será configurado aqui
+        )
+    ),
+})
